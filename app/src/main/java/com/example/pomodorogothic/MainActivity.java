14b5ffb508925afe6272b6a696b7474b084a63ec
Kambyrlee Heath <kambyrlee.heath@gmail.com>
2026-05-20 18:00:44 -0600

    added bg gradient and basic timer functionality

--- app/src/main/java/com/example/pomodorogothic/MainActivity.java
+++ app/src/main/java/com/example/pomodorogothic/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.pomodorogothic;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,9 +16,19 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.pomodorogothic.databinding.ActivityMainBinding;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private TextView timerText;
+    private Button  start_stop;
+
+    private CountDownTimer timer;
+    private boolean timerIsActive = false;
+    private static final long TIMER_SET_MS = 1500000;
+    private long timeRemaining = TIMER_SET_MS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +46,61 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        timerText = findViewById(R.id.timerText);
+        start_stop = findViewById(R.id.start_stop);
+
+        start_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (timerIsActive){
+                    stopTimer();
+                }
+                else {
+                    startTimer();
+                }
+            }
+        });
+
+        updateTimerText();
+    }
+
+    private void startTimer() {
+        timer = new CountDownTimer(timeRemaining, 1000) {
+            @Override
+            public void onFinish() {
+                timerIsActive = false;
+                start_stop.setText(getString(R.string.start_button_text));
+                timeRemaining = TIMER_SET_MS;
+                updateTimerText();
+            }
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeRemaining = millisUntilFinished;
+                updateTimerText();
+            }
+        }.start();
+
+        timerIsActive = true;
+        start_stop.setText(getString(R.string.stop_button_text));
+    }
+
+    private void stopTimer() {
+        if (timer != null) {
+            timer.cancel();
+        }
+        timerIsActive = false;
+        start_stop.setText(getString(R.string.start_button_text));
+    }
+
+    private void updateTimerText() {
+        int minutes = (int) (timeRemaining / 1000) / 60;
+        int seconds = (int) (timeRemaining / 1000) % 60;
+
+        String formattedTime = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        timerText.setText(formattedTime);
+
     }
 
 }
